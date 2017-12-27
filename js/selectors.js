e7e2124ae3d7de3b85ba5a532f4c2375c7ecaef9
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-27 05:00:30 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,12 @@
 // declare your functions here...
 function paragraphSelector() {
-  retrun $('p')
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
 }
